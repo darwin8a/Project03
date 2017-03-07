09e8d7a45add43c57e77bc83d76683c6bf204ab6
ckurland <cekurland@yahoo.com>
2017-03-07 14:52:43 -0800

    Update ckurland.cpp

--- ckurland.cpp
+++ ckurland.cpp
@@ -29,18 +29,31 @@ cout << "Your Health: " << bear.health << endl;
   case 2:
   cout << "You pick up the sharp rock and charge the bear." << endl;
   cout << "After dodging most of the bear's swipes, you make it to striking range and hit the bear repeatedly with the rock." << endl;
-  cout << "After a brief fight, the bear collapses in defeat afte rbeig slain by you." << endl;
+  cout << "After a brief fight, the bear collapses in defeat after being slain by you." << endl;
   cout << "With the bear just lying there, you decide to skin it and use the fur as a jacket." << endl;
   bear.fur = true;
-  
+  cout << "After skinning the bear, you take the fur and leave the cave with minimal health loss." << endl;
+  bear.health-=2;
+  cout << "Your health: " << bear.health << endl;
+  break;
 }
 if (bear_choice == 2)
 {
-cout << "As you turn around and run, the bear hits you from behind, dealing some damae, but you make it out alive." << endl;
+cout << "As you turn around and run, the bear hits you from behind, dealing some damage, but you make it out alive." << endl;
 bear.health-=4;
 cout << "Your Health: " << bear.health << endl;
 }
-
-
+cout << "Once leaving the cave, you realize it is getting dark and you don't have a place to sleep." << endl;
+cout << "Still scared taht the bear might come after you, you have to decide whether to try and make a fire or just climb into a tree and sleep there." << endl;
+  cout<< "Would you like to 1) Try and make a fire or 2) Climb into the tree?" << endl;
+  int shelter;
+  cin >> shelter;
+  if (shelter == 1)
+  {
+   cout << "After looking for materials, you find that there isn't anything to make fire with." << endl;
+    cout << "This leaves you with one option, climb into the tree." << endl;
+  }
+cout << "Once making the climb into the tree, you find a nice place to rest," << endl;
+  cout << "and with the warmth of the bear skin, you fall asleep, hoping the bear stays away." << endl;
 
 }
