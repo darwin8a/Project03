cd5a0f1bf603dc5bfa4c29f331dcfc05b97380aa
Asterisk007 <spykid1998@gmail.com>
2017-03-11 20:05:38 -0800

    Update group01.cpp
    Add endgame evaluation logic and message.

--- group01.cpp
+++ group01.cpp
@@ -15,5 +15,18 @@ int main() {
   h = ckurland(h);
   h = dochoa(h);
   h = cluibel(h);
+  cout << "Let's see how you did:\n";
+  cout << "Health: " << h.health << endl;
+  if (h.health >= 6){
+    cout << "Looks like you made it out okay.";
+  }
+  else if (h.health == 5){
+    cout << "Pretty damaged, but you'll recover within a few days." << endl;
+  }
+  else {
+    cout << "Your wounds are deep and you appear badly injured.\nThis did not go well for you." << endl;
+  }
+  cout << "Your adventure concludes here, but can you do better? If you made it out okay," << endl;
+  cout << "you did very well. Otherwise, see how little health you can lose." << endl;
   return 0;
 }
